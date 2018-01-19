20a05a7b00722f2b3815c559a5f4aee298c142bf
qct <quchentao@163.com>
2018-01-19 22:35:18 +0800

    init

---
+++ src/main/java/com/example/devopsdemo/DevopsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.devopsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDemoApplication.class, args);
+	}
+}
